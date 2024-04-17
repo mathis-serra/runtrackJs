afa3df3525c7d117a642b22fee4299f59b7521d6
mathis-serra <mathis.serra@laplateforme.io>
2024-04-17 09:24:23 +0200

    correct job04

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -1,12 +1,11 @@
 document.addEventListener('keydown', function(event) {
     var textarea = document.getElementById('keylogger');
-    var key = event.key.toLowerCase(); // Convertir en minuscule pour uniformiser
-  
-    // Vérifier si la touche pressée est une lettre de l'alphabet (a-z)
+    var key = event.key.toLowerCase(); 
+
     if (/^[a-z]$/.test(key)) {
-      // Si le focus est dans le textarea, ajouter la lettre deux fois
+      
       if (document.activeElement === textarea) {
-        textarea.value += key + key;
+        textarea.value += key;
       } else {
         textarea.value += key;
       }
